393fff6856da6149d069b1d690c4d0d8addabf0f
keet <nikolay1337@icloud.com>
2018-12-16 03:57:38 +0300

    Table impl

--- customtreeitem.h
+++ customtreeitem.h
@@ -3,6 +3,7 @@
 
 #include <QTreeWidgetItem>
 #include <QLineEdit>
+#include <QTableWidget>
 #include "clickablelabel.h"
 #include "idmanager.h"
 
@@ -10,13 +11,19 @@ class CustomTreeItem : public QObject, public QTreeWidgetItem
 {
     Q_OBJECT
 public:
-    CustomTreeItem(QString name, QTreeWidget* tree, unsigned int m_level = 1);
+    CustomTreeItem(QString name, QTreeWidget* tree, QTableWidget* table, unsigned int m_level = 1);
     void SetLineEdit();
     void SetLabel();
     void FirstSetLabel();
+    void initTable(QTableWidget* );
+    void addToTable(CustomTreeItem* );
+    void sumTableValues();
+    void deleteFromTable(CustomTreeItem* );
+    void renameTableItem(CustomTreeItem* );
     unsigned int GetItemId() const;
     unsigned int GetLevelId() const;
     QString GetLabel() const;
+    QTableWidget* m_table;
     ~CustomTreeItem();
 public slots:
     void makeEditable();
